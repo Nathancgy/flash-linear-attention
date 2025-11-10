18d3fb2d264a8c5b7a911c8f241c912d6b000fc4
Nathancgy4 <guangyu.chen40730-biph@basischina.com>
2025-11-10 12:32:16 +0000

    removed unnecessary files

--- sba_code/load_model_with_dolomite_demo.py
+++
@@ -1,7 +0,0 @@
-import transformers
-from dolomite_engine import hf_models
-
-if __name__ == "__main__":
-    hf_models = transformers.AutoModelForCausalLM.from_pretrained(
-            'shawntan/stickbreaking-3b',
-    )
